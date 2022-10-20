8549afaffd1f7479635e857a15d1839c36c560d2
Tintin <tintin@MacBook-Pro.local>
2022-10-20 22:35:49 +0800

    new: 设计稿适配-轮播图

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: avoid_print
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'routers/router.dart';
 
@@ -16,24 +17,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      // home: const MyHomePage(title: 'Flutter Demo'),
-      // home: const Tabs(),
-      initialRoute: '/',
-      onGenerateRoute: onGenerateRoute, // 命名路由
-      debugShowCheckedModeBanner: false, // 去掉debug图标
-    );
+    return ScreenUtilInit(
+        designSize: const Size(750, 1334), // 配置设计稿的宽度高度
+        builder: () => MaterialApp(
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                // Try running your application with "flutter run". You'll see the
+                // application has a blue toolbar. Then, without quitting the app, try
+                // changing the primarySwatch below to Colors.green and then invoke
+                // "hot reload" (press "r" in the console where you ran "flutter run",
+                // or simply save your changes to "hot reload" in a Flutter IDE).
+                // Notice that the counter didn't reset back to zero; the application
+                // is not restarted.
+                primarySwatch: Colors.blue,
+              ),
+              // home: const MyHomePage(title: 'Flutter Demo'),
+              // home: const Tabs(),
+              initialRoute: '/',
+              onGenerateRoute: onGenerateRoute, // 命名路由
+              debugShowCheckedModeBanner: false, // 去掉debug图标
+            ));
   }
 }
 
--- lib/pages/tabs/Home.dart
+++ lib/pages/tabs/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,19 +10,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // 轮播图
+  Widget _swiperWidget() {
+    List<Map> imgList = [
+      {"url": "https://www.itying.com/images/flutter/slide01.jpg"},
+      {"url": "https://www.itying.com/images/flutter/slide02.jpg"},
+      {"url": "https://www.itying.com/images/flutter/slide03.jpg"},
+    ];
+
+    return AspectRatio(
+      aspectRatio: 2 / 1,
+      child: Swiper(
+        itemBuilder: (BuildContext context, index) =>
+            Image.network(imgList[index]["url"], fit: BoxFit.fill),
+        itemCount: imgList.length,
+        pagination: const SwiperPagination(),
+        autoplay: true,
+      ),
+    );
+  }
+
+  // 标题
+  Widget _titleWidget(value) {
+    return Container(
+      height: ScreenUtil().setHeight(34),
+      margin: EdgeInsets.only(left: ScreenUtil().setWidth(20)),
+      padding: EdgeInsets.only(left: ScreenUtil().setHeight(20)),
+      decoration: BoxDecoration(
+          border: Border(
+              left: BorderSide(
+                  color: Colors.red, width: ScreenUtil().setWidth(10)))),
+      child: Text(
+        value,
+        style:
+            TextStyle(color: Colors.black54, fontSize: ScreenUtil().setSp(26)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/search');
-              },
-              child: const Text("跳转到搜索"))
-        ],
-      ),
+    return ListView(
+      children: <Widget>[
+        _swiperWidget(),
+        const SizedBox(
+          height: 10,
+        ),
+        _titleWidget("猜你喜欢"),
+        const SizedBox(
+          height: 10,
+        ),
+        _titleWidget("热门推荐")
+      ],
     );
   }
 }
