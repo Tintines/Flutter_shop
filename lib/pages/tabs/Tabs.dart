6c5ff81a27ddaac351ecc58736973f9df140df54
Tintin <tintin@MacBook-Pro.local>
2022-11-03 21:13:43 +0800

    tabs: 顶部假搜索框

--- lib/pages/tabs/Tabs.dart
+++ lib/pages/tabs/Tabs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../services/ScreenAdapter.dart';
 
 import 'Home.dart';
 import 'Category.dart';
@@ -31,9 +32,57 @@ class _TabsState extends State<Tabs> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter_shop'),
-      ),
+      appBar: _currentIndex != 3
+          ? AppBar(
+              leading: const IconButton(
+                icon: Icon(
+                  Icons.center_focus_weak,
+                  size: 28,
+                  color: Colors.black87,
+                ),
+                onPressed: null,
+              ),
+              title: InkWell(
+                child: Container(
+                  height: ScreenAdapter.height(60),
+                  decoration: BoxDecoration(
+                      color: const Color.fromRGBO(233, 233, 233, 0.8),
+                      borderRadius: BorderRadius.circular(30)),
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Row(
+                    children: [
+                      const Icon(Icons.search, color: Colors.black87),
+                      Text(
+                        "笔记本",
+                        style: TextStyle(
+                            fontSize: ScreenAdapter.size(28),
+                            color: Colors.black87),
+                      )
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                  ),
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, "/search");
+                },
+              ),
+              actions: const [
+                IconButton(
+                  icon: Icon(
+                    Icons.message,
+                    size: 28,
+                    color: Colors.black87,
+                  ),
+                  onPressed: null,
+                )
+              ],
+            )
+          : AppBar(
+              title: const Text(
+                "用户中心",
+                style: TextStyle(color: Colors.black87),
+              ),
+            ),
       // LIGHT: 使用AutomaticKeepAliveClientMixin 进行状态保持, 必须使用 PageView 进行包裹
       body: PageView(
         controller: _pageController,
