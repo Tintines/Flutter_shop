36081a265eeae3f4ed9c8af457a0b0439197f61a
Tintin <tintin@MacBook-Pro.local>
2022-11-03 21:50:31 +0800

    new: 搜索页面tabBar布局

--- lib/pages/Search.dart
+++ lib/pages/Search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../services/ScreenAdapter.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -12,7 +13,40 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("搜索页面"),
+        title: Container(
+          child: TextField(
+            autofocus: true,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none),
+                contentPadding:
+                    const EdgeInsets.only(left: 20, right: 20)), // 垂直居中
+          ),
+          height: ScreenAdapter.height(60),
+          decoration: BoxDecoration(
+            color: const Color.fromRGBO(233, 233, 233, 0.8),
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        actions: <Widget>[
+          InkWell(
+            child: SizedBox(
+              height: ScreenAdapter.height(60),
+              width: ScreenAdapter.width(100),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Text(
+                    "搜索",
+                    style: TextStyle(color: Colors.black87),
+                  )
+                ],
+              ),
+            ),
+            onTap: () {},
+          )
+        ],
       ),
       body: const Text('搜索'),
     );
